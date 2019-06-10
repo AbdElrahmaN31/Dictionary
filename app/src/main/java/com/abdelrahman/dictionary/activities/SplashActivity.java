601afeb8befa8fa28c977b4d5e748fc1384c089e
AbdElrahmaN31 <abdelrahmannashaat0@gmail.com>
2019-06-10 03:57:45 +0200

    add app icon and splash screen

---
+++ app/src/main/java/com/abdelrahman/dictionary/activities/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.abdelrahman.dictionary.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.abdelrahman.dictionary.R;
+import com.abdelrahman.dictionary.activities.main.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
